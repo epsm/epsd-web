e96833a3d8b12ae317e8c7bb4a005e84d059a21b
valeriy <epsm@i.ua>
2016-01-14 15:57:57 +0200

    statrting removing

--- src/main/java/com/epsm/epsdWeb/configuration/WebConfig.java
+++ src/main/java/com/epsm/epsdWeb/configuration/WebConfig.java
@@ -9,5 +9,5 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableWebMvc
 @ComponentScan("com.epsm.epsdWeb.controller")
 public class WebConfig extends WebMvcConfigurerAdapter{
-	
+
 }
