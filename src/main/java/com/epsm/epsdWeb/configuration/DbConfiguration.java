f862175756fa9d1ea26674f5e3235580691ac40a
valeriy <epsm@i.ua>
2016-01-15 19:01:10 +0200

    Created two profiles: dev and prod.

--- src/main/java/com/epsm/epsdWeb/configuration/DbConfiguration.java
+++ src/main/java/com/epsm/epsdWeb/configuration/DbConfiguration.java
@@ -5,8 +5,10 @@ import java.util.Properties;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -15,13 +17,42 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
+@PropertySource("classpath:application.properties")
 public class DbConfiguration{
 
+	@Value("${database.url}")
+	private String databaseUrl;
+	
+	@Value("${database.username}")
+	private String user;
+	
+	@Value("${database.password}")
+	private String password;
+	
+	@Value("${database.driver}")
+	private String databaseDriver;
+	
+	@Value("${hibernate.dialect}")
+	private String hibernateDialect;
+	
+	@Value("${hbm2ddl.auto}")
+	private String hbm2ddlAuto;
+	
     @Bean
     public DataSource dataSource() {
+    	System.out.println("url=" + databaseUrl);
+    	System.out.println("usr=" + user);
+    	System.out.println("pwd=" + password);
+    	System.out.println("dbd=" + databaseDriver);
+    	System.out.println("hdl=" + hibernateDialect);
+    	System.out.println("hbm=" + hbm2ddlAuto);
+    	
         BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setUrl("jdbc:h2:mem:test_mem;DB_CLOSE_ON_EXIT=FALSE");
-        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl(databaseUrl);
+        dataSource.setUsername(user);
+        dataSource.setPassword(password);
+        dataSource.setDriverClassName(databaseDriver);
+        
         return dataSource;
     }
     
@@ -31,8 +62,8 @@ public class DbConfiguration{
         HibernateJpaVendorAdapter adaptor = new HibernateJpaVendorAdapter();
         Properties properties = new Properties();
         
-        properties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
-        properties.put("hibernate.hbm2ddl.auto", "create");
+        properties.put("hibernate.dialect", hibernateDialect);
+        properties.put("hibernate.hbm2ddl.auto", hbm2ddlAuto);
         factory.setJpaVendorAdapter(adaptor);
         factory.setDataSource(dataSource());
         factory.setPackagesToScan("com.epsm.epsdWeb.domain");
