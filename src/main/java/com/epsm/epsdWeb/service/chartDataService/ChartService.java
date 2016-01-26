9be23e33d634f002d7e9a7913940ae5f8d86efea
valeriy <epsm@i.ua>
2016-01-26 18:28:19 +0200

    ChartData class ready.

--- src/main/java/com/epsm/epsdWeb/service/chartDataService/ChartService.java
+++ src/main/java/com/epsm/epsdWeb/service/chartDataService/ChartService.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.epsm.epsdWeb.repository.SavedConsumerStateDao;
 import com.epsm.epsdWeb.repository.SavedGeneratorStateDao;
 
 @Service
--- src/main/java/com/epsm/epsdWeb/service/chartDataService/ChartsDataSource.java
+++ src/main/java/com/epsm/epsdWeb/service/chartDataService/ChartsDataSource.java
@@ -1,15 +1,48 @@
 package com.epsm.epsdWeb.service.chartDataService;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ChartsDataSource {
+	private LocalDate date;
+	private Map<String, String> chartsData =  new HashMap<String, String>();
 	
+	@Autowired
+	private DayDateValidator validator;
 	
 	public Map<String, String> getDataForChartsOnDate(LocalDate date){
-		return null;
+		saveDate(date);
+		clearMap();
+		
+		if(isThereDataOnDate()){
+			fillChartsData();
+		}
+		
+		return chartsData;
+	}
+	
+	private void saveDate(LocalDate date){
+		this.date = date;
+	}
+	
+	private void clearMap(){
+		chartsData.clear();
+	}
+	
+	private boolean isThereDataOnDate(){
+		return validator.isDataOnDateFull(date);
+	}
+	
+	private void fillChartsData(){
+		
+	}
+	
+	private void fillFrequencyChartData(){
+		
 	}
 }
