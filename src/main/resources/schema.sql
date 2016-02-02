402c3eeb886c4c4ed047681b86c5da0fd1befca0
valeriy <epsm@i.ua>
2016-02-02 20:27:44 +0200

    Sql schema fixed.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS generator_state (
 	power_object_id bigint(20),
 	real_timestamp datetime NOT NULL,
 	frequency float,
-	generation_in_MW float,
+	generation_in_mw float,
 	generator_number int(11),
 	PRIMARY KEY (id),
 	UNIQUE KEY OneEntryInOneMomentForGenerator (power_object_id,generator_number,power_object_date,power_object_time)
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS consumer_state (
 	power_object_time time NOT NULL,
 	power_object_id bigint(20),
 	real_timestamp datetime NOT NULL,
-	load_in_MW float,
+	load_in_mw float,
 	PRIMARY KEY (id),
 	UNIQUE KEY OneEntryInOneMomentForConsumer (power_object_id,power_object_date,power_object_time)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -32,11 +32,11 @@ CREATE TABLE IF NOT EXISTS user (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE OR REPLACE VIEW total_generation AS SELECT
-	id, power_object_date, power_object_time, sum(generation_in_mw) AS total_generation_in_MW
+	id, power_object_date, power_object_time, sum(generation_in_mw) AS total_generation_in_mw
 	FROM generator_state GROUP BY power_object_date, power_object_time;
 
 CREATE OR REPLACE VIEW total_consumption AS SELECT
-	id, power_object_date, power_object_time, sum(load_in_mw) AS total_consumption_in_MW
+	id, power_object_date, power_object_time, sum(load_in_mw) AS total_consumption_in_mw
 	FROM consumer_state GROUP BY power_object_date, power_object_time;
 
 CREATE OR REPLACE VIEW frequency AS SELECT id, power_object_date, power_object_time, frequency 
