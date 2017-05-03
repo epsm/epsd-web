4578def18aa946b707895300068957ef2aca0ab8
epsm <epsm@i.ua>
2017-05-04 00:01:56 +0300

    make test commit to DB

--- src/test/java/com/epsm/epsdweb/repository/AbstractDaoTest.java
+++ src/test/java/com/epsm/epsdweb/repository/AbstractDaoTest.java
@@ -3,6 +3,7 @@ package com.epsm.epsdweb.repository;
 import com.epsm.epsdweb.configuration.TestDbConfiguration;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
@@ -10,10 +11,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes={TestDbConfiguration.class})
+@ContextConfiguration(classes = {TestDbConfiguration.class})
 @Transactional
 @Ignore
 @SqlGroup({@Sql(value = {"classpath:test-clean.sql"}), @Sql})
+@Rollback(false)
 public class AbstractDaoTest{
 
 }
